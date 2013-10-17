b57874c887c587ffe33014657a881d80241ed1b4
Oktay Acikalin <oktay.acikalin@gmail.com>
2013-10-17 07:47:00 +0200

    Moved csv value parsing into config module.

--- lib/config.py
+++ lib/config.py
@@ -3,13 +3,34 @@ try:
 except ImportError:
     import ConfigParser as configparser
 from os.path import join, exists, expanduser
+import csv
 
 
 USER_CONFIG_DIR = '~/.config/cronotrigger'
 
 
+class ConfigParser(configparser.ConfigParser):
+
+    def __csv_parse_value(self, value, list=True):
+        parser = csv.reader([value], delimiter=',', quotechar='"', skipinitialspace=True)
+        for fields in parser:
+            return fields if list else fields[0]
+
+    def get(self, *args, **kwargs):
+        kwargs['raw'] = True
+        # Python 2 ConfigParser is an old school class type - cannot use super.
+        value = configparser.ConfigParser.get(self, *args, **kwargs)
+        return self.__csv_parse_value(value, False)
+
+    def getlist(self, *args, **kwargs):
+        kwargs['raw'] = True
+        # Python 2 ConfigParser is an old school class type - cannot use super.
+        value = configparser.ConfigParser.get(self, *args, **kwargs)
+        return self.__csv_parse_value(value, True)
+
+
 def get_config(user_config):
-    config = configparser.ConfigParser()
+    config = ConfigParser()
     config.read('default.ini')
 
     user_filepath = expanduser(join(USER_CONFIG_DIR, user_config))
