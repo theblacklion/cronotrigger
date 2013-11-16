347e8ff2be8abf973dacebfd13af838b8fea8963
Oktay Acikalin <oktay.acikalin@gmail.com>
2013-11-16 22:02:23 +0100

    Implemented split of chunks into smaller parts which also copies sparse files more precisely.

--- lib/backup.py
+++ lib/backup.py
@@ -18,9 +18,11 @@ from lib.dtree import scan, copystat
 CHUNK_SIZE = 1024 * 1024 * 5  # Bytes
 QUEUE_SIZE = 25  # Length
 
-CHUNK_SPARSE_DATA = b'\0' * CHUNK_SIZE
-CHUNK_TYPE_SPARSE = None
 CHUNK_TYPE_EMPTY = 0
+
+CHUNK_PART_SIZE = 64 * 1024  # Bytes
+CHUNK_PART_SPARSE_DATA = b'\0' * CHUNK_PART_SIZE
+CHUNK_PART_TYPE_SPARSE = None
 
 
 class Reader(Thread):
@@ -37,10 +39,27 @@ class Reader(Thread):
     def add_more_bytes(self, count):
         self._sum_bytes += count
 
+    def _read_chunk(self, handle, detect_sparse=False):
+        cur_size = 0
+        chunk = []
+        chunk_append = chunk.append
+        handle_read = handle.read
+        while cur_size < CHUNK_SIZE:
+            part = handle_read(CHUNK_PART_SIZE)
+            part_len = len(part)
+            if not part_len:
+                break
+            if detect_sparse and part_len == CHUNK_PART_SIZE and part == CHUNK_PART_SPARSE_DATA:
+                part = CHUNK_PART_TYPE_SPARSE
+            chunk_append(part)
+            cur_size += part_len
+        return chunk, cur_size
+
     def run(self):
         self._logger.debug('Started thread.')
         sum_bytes = self._sum_bytes
         sum_bytes_transferred = 0
+        read_chunk = self._read_chunk
         while self._running:
             try:
                 item = self._input_queue.get(timeout=0.1)
@@ -134,8 +153,7 @@ class Reader(Thread):
                             if size >= CHUNK_SIZE:
                                 if os_fstat(handle.fileno()).st_blocks * 512 < size:
                                     detect_sparse = True
-                            chunk = handle.read(CHUNK_SIZE)  # read chunk
-                            chunk_len = len(chunk)
+                            chunk, chunk_len = read_chunk(handle, detect_sparse=detect_sparse)  # read chunk
                             bytes_transferred = 0
                             while chunk_len and self._running:
                                 bytes_transferred += chunk_len
@@ -146,8 +164,6 @@ class Reader(Thread):
                                                 sum_bytes_transferred)
                                                if sum_bytes else 0)
                                 sum_hsize = human_size(sum_bytes)
-                                if detect_sparse and chunk_len == CHUNK_SIZE and chunk == CHUNK_SPARSE_DATA:
-                                    chunk = CHUNK_TYPE_SPARSE
                                 self._output_queue.put(dict(
                                     type='file',
                                     src_dir=src_dir,
@@ -158,8 +174,7 @@ class Reader(Thread):
                                            (percent, hsize, sum_percent,
                                             sum_hsize),
                                 ))
-                                chunk = handle.read(CHUNK_SIZE)  # read more
-                                chunk_len = len(chunk)
+                                chunk, chunk_len = read_chunk(handle, detect_sparse=detect_sparse)  # read more
                         self._output_queue.put(dict(
                             type='meta',
                             src_dir=src_dir,
@@ -200,9 +215,21 @@ class Writer(Thread):
         self._num_symlinks = 0
         self._logger = logging.getLogger('backup.writer')
 
+    def _write_chunk(self, handle, chunk):
+        # print('got %d parts in chunk' % len(chunk))
+        for part in chunk:
+            if part is not CHUNK_PART_TYPE_SPARSE:
+                # print('NORMAL')
+                handle.write(part)
+            else:
+                # print('SPARSE')
+                handle.seek(CHUNK_PART_SIZE, 1)  # 1 means cur file pos.
+                # print(handle.tell())
+
     def run(self):
         self._logger.debug('Started thread.')
         handle = None
+        write_chunk = self._write_chunk
         while self._running:
             try:
                 item = self._input_queue.get(timeout=0.1)
@@ -250,20 +277,18 @@ class Writer(Thread):
                             self._num_files += 1
                             self._logger.debug('Created file: %s' % dst_file)
                         elif dst_file != handle.name:
+                            handle.truncate()
                             handle.close()
                             handle = open(dst_file, 'wb')
                             self._num_files += 1
                             self._logger.debug('Created file: %s' % dst_file)
                         if data is CHUNK_TYPE_EMPTY:
                             pass  # Nothing to write.
-                        elif data is not CHUNK_TYPE_SPARSE:
-                            # print('NORMAL')
-                            handle.write(data)
                         else:
-                            # print('SPARSE')
-                            handle.seek(CHUNK_SIZE, 1)  # 1 means cur file pos.
+                            write_chunk(handle, data)
                     elif type_ == 'meta':
                         if handle and handle.name == dst_file:
+                            handle.truncate()
                             handle.close()
                             handle = None
                         copystat(data, dst_file, follow_symlinks=False)
@@ -277,6 +302,7 @@ class Writer(Thread):
                 self._is_idle = True
                 time.sleep(0.1)
         if handle:
+            handle.truncate()
             handle.close()
         self._logger.debug('Stopped thread.')
         self._is_idle = True
